b9ba4c71aa2c632c7ea8f81caba22912e5a6b51f
bhalouane <bhalouane@yahoo.fr>
2024-12-06 16:22:49 +0000

    fix: attempts to make it work on idx

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.pnpm
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+
+  services = {
+    docker = {
+      enable =  true;
+    };
+  };
+}
